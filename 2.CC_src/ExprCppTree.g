d168ed7d9e64543ea17ee6f3642e38dd5725648a
sunowsir <sunowsir@qq.com>
2019-03-15 17:31:21 +0800

    update some code

--- 2.CC_src/ExprCppTree.g
+++ 2.CC_src/ExprCppTree.g
@@ -6,6 +6,17 @@ options {
     ASTLabelType=pANTLR3_BASE_TREE;
 }
 
+/* 虚拟标记 ；
+ * 注意topens{}定义的位置，
+ * 放在@header{}下面会报错，原因未知。。
+ */
+tokens {
+    TOK_BLOCK;
+    TOK_IF;
+    TOK_ELIF;
+    TOK_ELSE;
+}
+
 @header {
     #include <assert.h>
 }
@@ -13,58 +24,111 @@ options {
 atom: 
     INT | 
     ID | 
-    PARENTHESES_LEFT! commaExpr PARENTHESES_RIGHT!
-    ;
+    PARENTHESES_LEFT! commaExpr PARENTHESES_RIGHT!;
 
-multExpr: 
-    atom (TIMES^ atom)*;
+notExpr:
+    (OPR_NOT^)* atom;
 
-expr: 
-    multExpr ((PLUS^ | MINUS^) multExpr)*;
+timesExpr: 
+    notExpr ((OPR_MULT^ | OPR_DIVISOR^ | OPR_PERCENT^) notExpr)*;
+
+orderExpr: 
+    timesExpr ((OPR_PLUS^ | OPR_MINUS^) timesExpr)*;
+
+unbalanceExpr: 
+    orderExpr ((OPR_LT^ | OPR_LE^ | OPR_GT^ | OPR_GE^) orderExpr)*;
+
+balanceExpr: 
+    unbalanceExpr ((OPR_EQ^ | OPR_NE^) unbalanceExpr)*;
+
+bitAndExpr:
+    balanceExpr (OPR_BIT_AND balanceExpr)*;
+
+xorExpr: 
+    bitAndExpr (OPR_XOR^ bitAndExpr)*;
+
+bitOrExpr:
+    xorExpr (OPR_BIT_OR^ xorExpr)*;
+
+andExpr:
+    bitOrExpr (OPR_AND^ bitOrExpr)*;
+
+orExpr:
+    andExpr (OPR_OR^ andExpr)*;
 
 assignExpr: 
-    (ID ASSIGN^)* expr;
+    (ID OPR_ASSIGN^)* orExpr;
 
 commaExpr:
-    assignExpr (COMMA^ assignExpr)*;
+    assignExpr (OPR_COMMA^ assignExpr)*;
 
 stmt: 
     commaExpr ENDLINE -> commaExpr | 
-    ENDLINE -> | 
-    block
-    ;
+    block | 
+    ENTER -> |
+    ENDLINE -> ;
 
 block_base: 
     (stmt)*;
 
 block: 
-    CURLY_BRACE_LEFT block_base CURLY_BRACE_RIGHT ENDBLOCK -> ^(BLOCK block_base);
+    CURLY_BRACE_LEFT block_base CURLY_BRACE_RIGHT ENDLINE -> ^(TOK_BLOCK block_base);
+
+mainBlock: 
+    KW_MAIN! block;
 
 prog: (
-    block {
-        pANTLR3_STRING s = $block.tree->toStringTree($block.tree);
+    mainBlock {
+        pANTLR3_STRING s = $mainBlock.tree->toStringTree($mainBlock.tree);
         assert(s->chars);
-        printf("tree \%s\n", s->chars);
+        printf("\033[1;32mtree \%s\n\033[0;m", s->chars);
     }
 )+;
 
+
+
+
+
+/* Keywords */
+
+KW_MAIN: 'main';
+KW_IF: 'if';
+KW_ELIF: 'elif';
+KW_ELSE: 'else';
+
+
+/* Operators */
+
+/*18*/OPR_COMMA: ',';
+/*16*/OPR_ASSIGN: '=';
+/*14*/OPR_OR: '||';
+/*13*/OPR_AND: '&&';
+/*12*/OPR_BIT_OR: '|';
+/*11*/OPR_XOR: '^';
+/*10*/OPR_BIT_AND: '&';
+/* 9*/OPR_EQ: '==';
+/* 9*/OPR_NE: '!=';
+/* 8*/OPR_LT: '<';
+/* 8*/OPR_LE: '<=';
+/* 8*/OPR_GT: '>';
+/* 8*/OPR_GE: '>=';
+/* 6*/OPR_PLUS: '+';
+/* 6*/OPR_MINUS: '-';
+/* 5*/OPR_MULT: '*';
+/* 5*/OPR_DIVISOR: '/';
+/* 5*/OPR_PERCENT: '%';
+/* 3*/OPR_NOT: '!';
 
 PARENTHESES_LEFT: '(';
 PARENTHESES_RIGHT: ')';
 CURLY_BRACE_LEFT: '{';
 CURLY_BRACE_RIGHT: '}';
-BLOCK: '{}';
 
-COMMA: ',';
-TIMES: '*';
-ASSIGN: '=';
-PLUS: '+';
-MINUS: '-';
 
 ID: ('a'..'z'|'A'..'Z')+ ;
 INT: '~'? '0'..'9'+ ;
 
-ENDLINE: ';'+ '\n';
-ENDBLOCK: '\n';
+ENDLINE: '\r'? ';'+ '\n';
+ENTER: '\n';
 
 WS : (' '|'\t')+ {$channel = HIDDEN;};
--- 2.CC_src/main.cpp
+++ 2.CC_src/main.cpp
@@ -10,18 +10,127 @@ using std::string;
 using std::cout;
 
 class ExprTreeEvaluator {
+private:
     map<string,int> memory;
+    ExprTreeEvaluator *next;
 public:
     ExprTreeEvaluator (ExprTreeEvaluator *next);
     int run(pANTLR3_BASE_TREE);
-    ExprTreeEvaluator *next;
     int get_Value(string search_value);
 };
+
 
 pANTLR3_BASE_TREE getChild(pANTLR3_BASE_TREE, unsigned);
 const char* getText(pANTLR3_BASE_TREE tree);
 
-int main (int argc, char* argv[]) {
+
+ExprTreeEvaluator::ExprTreeEvaluator (ExprTreeEvaluator *next) {
+    this->next = next;
+}
+
+int ExprTreeEvaluator::get_Value (string search_value) {
+    if (this->memory.find(search_value) != this->memory.end()) {
+        return this->memory[search_value];
+    } else if (this->next != NULL) {
+        return this->next->get_Value(search_value);
+    } 
+    std::cerr << "\033[1;31mNo found variable \"" << search_value << "\"\033[0;m\n";
+    exit (-1);
+}
+
+int ExprTreeEvaluator::run (pANTLR3_BASE_TREE tree) {
+    pANTLR3_COMMON_TOKEN tok = tree->getToken(tree);
+    if(tok) {
+        switch(tok->type) {
+            case TOK_BLOCK : {
+                ExprTreeEvaluator new_poj(this);
+                int tree_num = tree->getChildCount(tree);
+                int run_result = 0;
+                for (int i = 0; i < tree_num; i++) {
+                    run_result = new_poj.run(getChild(tree, i));
+                    cout << "value : " << run_result << "\n";
+                }
+                return run_result;
+            }
+            case INT: {
+                const char* s = getText(tree);
+                if (s[0] == '~') {
+                    return -atoi(s+1);
+                } else {
+                    return atoi(s);
+                }
+            }
+            case ID: {
+                string var(getText(tree));
+                return get_Value(var);
+            }
+            case OPR_COMMA: 
+                return run(getChild(tree, 0)), run(getChild(tree, 1));
+            case OPR_ASSIGN: {
+                string var(getText(getChild(tree,0)));
+                int val = run(getChild(tree,1));
+                memory[var] = val;
+                return val;
+            }
+            case OPR_OR: 
+                return run(getChild(tree, 0)) || run(getChild(tree, 1));
+            case OPR_AND: 
+                return run(getChild(tree, 0)) && run(getChild(tree, 1));
+            case OPR_BIT_OR: 
+                return run(getChild(tree, 0)) | run(getChild(tree, 1));
+            case OPR_XOR: 
+                return run(getChild(tree, 0)) ^ run(getChild(tree, 1));
+            case OPR_BIT_AND: 
+                return run(getChild(tree, 0)) & run(getChild(tree, 1));
+            case OPR_EQ: 
+                return run(getChild(tree, 0)) == run(getChild(tree, 1));
+            case OPR_NE: 
+                return run(getChild(tree, 0)) != run(getChild(tree, 1));
+            case OPR_LT:
+                return run(getChild(tree, 0)) < run(getChild(tree, 1));
+            case OPR_LE:
+                return run(getChild(tree, 0)) <= run(getChild(tree, 1));
+            case OPR_GT:
+                return run(getChild(tree, 0)) > run(getChild(tree, 1));
+            case OPR_GE:
+                return run(getChild(tree, 0)) >= run(getChild(tree, 1));
+            case OPR_PLUS:
+                return run(getChild(tree, 0)) + run(getChild(tree, 1));
+            case OPR_MINUS:
+                return run(getChild(tree, 0)) - run(getChild(tree, 1));
+            case OPR_MULT:
+                return run(getChild(tree, 0)) * run(getChild(tree, 1));
+            case OPR_DIVISOR:
+                return run(getChild(tree, 0)) / run(getChild(tree, 1));
+            case OPR_PERCENT:
+                return run(getChild(tree, 0)) % run(getChild(tree, 1));
+            case OPR_NOT:
+                return !run(getChild(tree, 0));
+            default:
+                cout << "Unhandled token: #" << tok->type << '\n';
+                return -1;
+        }
+    } else {
+        int k = tree->getChildCount(tree);
+        int r = 0;
+        for (int i = 0; i < k; i++) {
+            r = run(getChild(tree, i));
+            cout << "value : " << r << "\n";
+        }
+        return r;
+    }
+}
+
+pANTLR3_BASE_TREE getChild (pANTLR3_BASE_TREE tree, unsigned i) {
+    assert(i < tree->getChildCount(tree));
+    return (pANTLR3_BASE_TREE) tree->getChild(tree, i);
+}
+
+const char* getText (pANTLR3_BASE_TREE tree) {
+    return (const char*) tree->getText(tree)->chars;
+}
+
+void Analysis_Start(int argc, char **argv) {
     pANTLR3_INPUT_STREAM input;
     pExprCppTreeLexer lex;
     pANTLR3_COMMON_TOKEN_STREAM tokens;
@@ -40,94 +149,17 @@ int main (int argc, char* argv[]) {
     pANTLR3_BASE_TREE tree = r.tree;
 
     ExprTreeEvaluator eval(NULL);
-    int rr = eval.run(tree);
-    cout << "Evaluator result: " << rr << '\n';
+    int res = eval.run(tree);
+    cout << "Evaluator result: " << res << '\n';
 
     parser->free(parser);
     tokens->free(tokens);
     lex->free(lex);
     input->close(input);
 
+}
+
+int main (int argc, char* argv[]) {
+    Analysis_Start(argc, argv);
     return 0;
-}
-
-int ExprTreeEvaluator::get_Value (string search_value) {
-    if (this->memory.find(search_value) != this->memory.end()) {
-        return this->memory[search_value];
-    } else if (this->next != NULL) {
-        return this->next->get_Value(search_value);
-    } 
-    return -1;
-}
-
-ExprTreeEvaluator::ExprTreeEvaluator(ExprTreeEvaluator *next) {
-    this->next = next;
-}
-
-int ExprTreeEvaluator::run(pANTLR3_BASE_TREE tree) {
-    pANTLR3_COMMON_TOKEN tok = tree->getToken(tree);
-    if(tok) {
-        switch(tok->type) {
-            case BLOCK : {
-                int tree_num = tree->getChildCount(tree);
-                int run_result = 0;
-                for (int i = 0; i < tree_num; i++) {
-                    run_result = run(getChild(tree, i));
-                }
-                return run_result;
-            }
-            case INT: {
-                const char* s = getText(tree);
-                if (s[0] == '~') {
-                    return -atoi(s+1);
-                } else {
-                    return atoi(s);
-                }
-            }
-            case ID: {
-                ExprTreeEvaluator now_prj(this);
-                string var(getText(tree));
-                int ret_value = now_prj.get_Value(var);
-                if (ret_value == -1) {
-                    std::cerr << "\033[1;31m undef variable \"" << var << "\"\033[0;m\n";
-                    exit (-1);
-                }
-                return ret_value;
-            }
-            case COMMA: 
-                return run(getChild(tree, 0)), run(getChild(tree, 1));
-            case PLUS:
-                return run(getChild(tree, 0)) + run(getChild(tree, 1));
-            case MINUS:
-                return run(getChild(tree, 0)) - run(getChild(tree, 1));
-            case TIMES:
-                return run(getChild(tree, 0)) * run(getChild(tree, 1));
-            case ASSIGN: {
-                string var(getText(getChild(tree,0)));
-                int val = run(getChild(tree,1));
-                memory[var] = val;
-                return val;
-            }
-            default:
-                cout << "Unhandled token: #" << tok->type << '\n';
-                return -1;
-        }
-    } else {
-        int k = tree->getChildCount(tree);
-        int r = 0;
-        for (int i = 0; i < k; i++) {
-            r = run(getChild(tree, i));
-            printf("value = %d\n", r);
-        }
-        return r;
-    }
-}
-
-pANTLR3_BASE_TREE getChild(pANTLR3_BASE_TREE tree, unsigned i) {
-    assert(i < tree->getChildCount(tree));
-    return (pANTLR3_BASE_TREE) tree->getChild(tree, i);
-}
-
-const char* getText(pANTLR3_BASE_TREE tree) {
-    return (const char*) tree->getText(tree)->chars;
 }
